d8b112f2a60bd1f2c7ad9d70c538fff742de1dfa
PixelbarEupen <info@pixelbar.be>
2014-04-10 14:58:29 +0200

    v0.1

--- index.php
+++ index.php
@@ -22,32 +22,13 @@
 	/******************************************************************************************/
 	/************************* DO NOT CHANGE ANYTHING AFTER THIS LINE *************************/
 	
-	//REGISTER NECESSARY FILES
-	function register_files(){
-		if(count(get_field($gallery_field_name)) > 1):
-			wp_register_script( 'cycle', plugins_url( 'js/cycle.js' , __FILE__ ));
-			wp_enqueue_script( 'cycle' );
-		endif;
-		
-		wp_register_style( 'fancybox-css', plugins_url( 'css/slideshow.js' , __FILE__ ));
-		wp_enqueue_style( 'fancybox-css' );
-	}
-	add_action( 'wp_enqueue_scripts', 'register_scripts', 100 );
+	include('includes/register.php');
+	include('includes/register-acf.php');
 	
-	
-	
-	
-	
-	if($use_with_hook ):
-		add_action( $hook_name, 'start_slideshow' );
-	endif;
+	//THE MAIN FUNCTION
 	function start_slideshow() { ?>
 		
-		<?php
-		
-			global $use_with_hook, $hook_name, $gallery_field_name, $image_size_name;
-		
-		?>
+		<?php global $use_with_hook, $hook_name, $gallery_field_name, $image_size_name;	?>
 		
 		<?php if(get_field($gallery_field_name)): ?>
 			<div class="home-slider">
@@ -82,4 +63,5 @@
 		<?php endif; ?>
 	
 	<?php }
+	add_shortcode('cycle-slideshow','start_slideshow');
 ?>
