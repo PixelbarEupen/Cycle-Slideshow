99a60b38bd1db00d88f07e16450eb8d383f65c81
PixelbarEupen <info@pixelbar.be>
2014-04-10 14:58:21 +0200

    v0.1

---
+++ includes/register.php
@@ -0,0 +1,15 @@
+<?php 
+	
+	//REGISTER NECESSARY FILES
+	function register_files(){
+		global $gallery_field_name; 
+		
+		if(count(get_field($gallery_field_name)) > 1):
+			wp_register_script( 'cycle', plugins_url( '../js/cycle.js' , __FILE__ ));
+			wp_enqueue_script( 'cycle' );
+		endif;
+		
+		wp_register_style( 'slideshow-css', plugins_url( '../css/slideshow.css' , __FILE__ ));
+		wp_enqueue_style( 'slideshow-css' );
+	}
+	add_action( 'wp_enqueue_scripts', 'register_files', 100 );
