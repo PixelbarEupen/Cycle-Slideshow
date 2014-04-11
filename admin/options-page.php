e3099c76fcf7cc5984dc89945cba3e1fcc3076a9
PixelbarEupen <info@pixelbar.be>
2014-04-11 08:53:06 +0200

    v0.1.6.1

---
+++ admin/options-page.php
@@ -0,0 +1,23 @@
+<div class="wrap">
+	<h2>Cycle Slideshow</h2>
+	<h3>Basic Options</h3>
+	<form method="post" action="options.php">
+		<?php settings_fields( 'super-settings-group' ); ?>
+		<?php do_settings( 'super-settings-group' ); ?>
+		<table class="form-table">
+			<tr valign="top">
+				<th scope="row">New Option Name</th>
+				<td><input type="text" name="new_option_name" value="<?php echo get_option('new_option_name'); ?>" /></td>
+			</tr>
+			<tr valign="top">
+				<th scope="row">Some Other Option</th>
+				<td><input type="text" name="some_other_option" value="<?php echo get_option('some_other_option'); ?>" /></td>
+			</tr>
+			<tr valign="top">
+				<th scope="row">Options, Etc.</th>
+				<td><input type="text" name="option_etc" value="<?php echo get_option('option_etc'); ?>" /></td>
+			</tr>
+		</table>
+		<?php submit_button(); ?>
+	</form>
+</div>
