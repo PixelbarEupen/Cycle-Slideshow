56afce51f4cde27963eabffe2e3ac02847cc32a7
PixelbarEupen <info@pixelbar.be>
2014-04-14 14:16:32 +0200

    v 0.1.7.1

--- includes/backend.php
+++ includes/backend.php
@@ -11,22 +11,22 @@ function cycle_slideshow_menu(){
     	add_options_page('Cycle Slideshow', 'Cycle Slideshow', 'manage_options', 'cycle-slideshow-menu', 'cycle_slideshow_options');
     	
     	//call register settings function
-		add_action( 'admin_init', 'register_mysettings' );
+		add_action( 'admin_init', 'cycle_slideshow_register_settings' );
 }
 add_action('admin_menu','cycle_slideshow_menu');
 
 //thi is the callback function from the menu element
 function cycle_slideshow_options(){
     	include(plugin_dir_path( __FILE__ ).'../admin/options-page.php');
-    	
 }
 
-
-function register_mysettings() {
-//register our settings
-	register_setting( 'cycle-settings', 'cycle-image-size' );
-	register_setting( 'cycle-settings', 'cycle-animation' );
-	register_setting( 'cycle-settings', 'cycle-timeout' );
-	register_setting( 'cycle-settings', 'cycle-show-pager');
-	register_setting( 'cycle-settings', 'cycle-pause');
+if(!function_exists('cycle_slideshow_register_settings')){
+	function cycle_slideshow_register_settings() {
+	//register our settings
+		register_setting( 'cycle-settings', 'cycle-image-size' );
+		register_setting( 'cycle-settings', 'cycle-animation' );
+		register_setting( 'cycle-settings', 'cycle-timeout' );
+		register_setting( 'cycle-settings', 'cycle-show-pager');
+		register_setting( 'cycle-settings', 'cycle-pause');
+	}
 }
